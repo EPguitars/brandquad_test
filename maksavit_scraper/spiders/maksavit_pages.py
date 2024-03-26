d5c026140402ff913f7f5c7d5c78a7a5d8ca09bf
EPguitars <pixel122@rambler.ru>
2024-03-26 11:41:08 +0700

    spider for pages with argument passing

---
+++ maksavit_scraper/spiders/maksavit_pages.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class MaksavitPagesSpider(scrapy.Spider):
+    name = "maksavit_pages"
+    allowed_domains = ["maksavit.ru"]
+    start_urls = ["https://maksavit.ru/"]
+    
+    def __init__(self, region=None, category=None, 
+                 subcategory=None, *args, **kwargs):
+        
+        super(MaksavitPagesSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f"https://maksavit.ru/{region}/catalog/{category}/{subcategory}"]
+
+    def parse(self, response):
+        print(response.status)
+        print("================")
+
+
+
+# scrapy crawl maksavit_pages -a region=novosibirsk
